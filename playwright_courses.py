514103b043613db96815599e92d0ab1953374bc8
Olya Radkovich <olga.radkovich@innowise.com>
2025-11-24 16:01:04 +0100

    Работа с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -8,10 +8,10 @@ with sync_playwright() as playwright:
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.namename@gmail.com')
+    email_input.fill('test.user@gmail.com')
 
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('Gargoyle')
+    username_input.fill('Tester')
 
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     password_input.fill('password')
@@ -26,4 +26,19 @@ with sync_playwright() as playwright:
     context = browser.new_context(storage_state='browser-state.json')
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
+
+    empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
+
+    empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
