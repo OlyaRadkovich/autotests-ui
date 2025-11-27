4c88ac30e555abf9b709ce74ca55ffb188ea9a4d
Olya Radkovich <olga.radkovich@innowise.com>
2025-11-27 10:47:55 +0100

    Маркировки Pytest

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,10 +1,21 @@
+import pytest
+
+SYSTEM_VAR = "42"
+
+
+@pytest.mark.xfail
 def test_first_try():
     print("Hello World!")
 
 
+@pytest.mark.skipif(
+    SYSTEM_VAR="42",
+    reason="that's the Answer to the Ultimate Question of Life, the Universe, and Everything"
+)
 def test_assert_positive_case():
     assert (2 + 2) == 4
 
 
+@pytest.mark.xfail
 def test_assert_negative_case():
     assert (2 + 2) == 5
