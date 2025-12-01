0e6719adb72e5278fa4bb42aa3be80b8a31a1b1e
Olya Radkovich <olga.radkovich@innowise.com>
2025-12-01 21:52:44 +0100

    Introduction to PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,24 +1,19 @@
 import pytest
-from playwright.sync_api import Page, expect
+
+from fixtures.pages import dashboard_page
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):
-    chromium_page.goto(
+def test_successful_registration(
+        registration_page: RegistrationPage,
+        dashboard_page: DashboardPage
+):
+    registration_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.namename@gmail.com')
-
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('Gargoyle')
-
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
+    registration_page.fill_registration_form()
+    registration_page.click_registration_button()
+    dashboard_page.check_visible_dashboard_title()
