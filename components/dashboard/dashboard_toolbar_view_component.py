0cc3ff6a7be0b7e902b26322f98d8e397741e5b7
Olya Radkovich <olga.radkovich@innowise.com>
2026-01-07 17:30:11 +0100

    PageFactory implementation practice

--- components/dashboard/dashboard_toolbar_view_component.py
+++ components/dashboard/dashboard_toolbar_view_component.py
@@ -1,12 +1,14 @@
 from components.base_component import BaseComponent
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+from elements.text import Text
+
 
 class DashboardToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        self.title = Text(page, 'dashboard-toolbar-title-text', 'Title')
 
     def check_visible(self):
-        expect(self.dashboard_title).to_be_visible()
-        expect(self.dashboard_title).to_have_text('Dashboard')
+        self.title.check_visible()
+        self.title.check_have_text('Dashboard')
